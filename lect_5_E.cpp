7db2bd4f60ce7527e4ef1e05f730b0271178811d
TIOros <tioros@mai.education>
2026-04-03 16:42:26 +0300

    l

---
+++ lect_5_E.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    bool flag = false;
+    int num;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n ; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n-1 ; i++){
+        if((arr[i] >= 0 && arr[i+1] >= 0) || (arr[i] < 0 && arr[i+1] < 0)){
+            flag = true;
+            break;
+        }
+    }
+    if(flag){
+        cout << "YES";
+    }
+    else cout << "NO";
+}
