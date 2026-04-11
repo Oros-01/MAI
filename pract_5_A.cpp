0525479316d7cb7fd6a022d9f1b540e46d129972
TIOros <tioros@mai.education>
2026-04-12 02:45:52 +0300

    p

---
+++ pract_5_A.cpp
@@ -0,0 +1,17 @@
+//обезьяна видит, обезъяна делает
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    
+    for(int i = 1; i < 2*1000000000; i++){
+        if(x/)
+
+
+    }
+
+
+    return 0;
+}
