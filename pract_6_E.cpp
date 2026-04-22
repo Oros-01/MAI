de1dbce3faf290bd61281d7bfe8de3c2d2a36a00
TIOros <tioros@mai.education>
2026-04-22 12:44:14 +0300

    p

--- pract_6_E.cpp
+++ pract_6_E.cpp
@@ -9,25 +9,23 @@ int main(){
     for(int i =0; i < size; i++){
         cin >> arr[i];
     }
-    int max = arr[0];
+    int maxIndex = 0;
+    int minIndex = 0;
+
     for(int i =0; i < size; i++){
-        if(arr[i] > max){
-            max = arr[i];
+        if(arr[i] > arr[maxIndex]){
+            maxIndex = i; 
         }
     }
     int min = arr[0];
     for(int i = 0; i < size; i++){
-        if(arr[i] < min){
-            min = arr[i];
+        if(arr[i] < arr[minIndex]){
+            minIndex = i;
         }
     }
+    swap(arr[minIndex], arr[maxIndex]);
+
     for(int i = 0; i < size; i++){
-        if(max == arr[i]){
-            if(min == arr[j]){
-                swap(arr[j], arr[i])
-            }
-        }
+        cout << arr[i] << " ";
     }
-    cout << max;
-    cout << min;
 }
