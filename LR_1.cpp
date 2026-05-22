b7b01e4354a532418f169baf70417d4060403734
TIOros <tioros@mai.education>
2026-05-22 04:22:35 +0300

    laba

--- LR_1.cpp
+++ LR_1.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 //ofstream для вывода в файл
 //ifstream для вывода в файл
 using namespace std;
 
-void classicBouble(int arr[], int size){
+/*void classicBubble(int arr[], int size){
     for(int i = 1; i < size; i++){
         for(int j = 0; j < size; j++){
             if(arr[i]<arr[j]){
                 swap(arr[i], arr[j]);
+            }
+        }
+    }
+}*/
+
+void classicBubble(int arr[], int size, long long &comps, long long &swaps) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1; j++) {
+            comps++;
+            if (arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swaps++;
             }
         }
     }
@@ -52,19 +65,30 @@ int main(){
     int size;
     cin >> size;
     int arr[size];
+    int bufer[size];
 
     for(int i = 0; i < size; i++){
         cin >> arr[i];
     }
-    for(int i = 0; i < size; i++){
+
+    long long comps, swaps;
+    ofstream out("results.csv");
+    out << "Algorithm;Comparisons;Swaps\n";
+
+
+    /*for(int i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
+    cout << endl; */
+
+    //классическая сортировка
+    memcpy(bufer, arr, size * sizeof(int));
+    classicBubble(bufer, size, comps, swaps);
+    out << "classic;" << comps << ";" << swaps << "\n";
+
     cout << endl;
 
-    classicBouble(arr, size);
-
-    cout << endl;
-
+    comps = 0; swaps = 0;
     for(int i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
